e46c08097a593ab443d6089e1dd19fc044285788
kdw <50583568+dong336@users.noreply.github.com>
2025-01-02 17:38:24 +0900

    UI 개발을 해보자

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    [SerializeField] bool isAnsweringQuestion = false;
+    float timerValue;
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer() {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion) {
+            if (timerValue <= 0) {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        } else {
+            if (timerValue <= 0) {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+
+        Debug.Log(timerValue);
+    }
+}
